dd0c550fc41f25867d3e2264f1d97d4260d2f2b7
viswanadh7 <mudunuruviswanadh@gmail.com>
2025-06-26 18:25:16 +0530

    feat(navbar): add basic Navbar component to RootLayout

---
+++ components/Navbar.tsx
@@ -0,0 +1,10 @@
+const Navbar = () => {
+    return (
+        <nav className="flex justify-between items-center h-16 px-10">
+            <h1 className="text-xl">Web blog</h1>
+            <button>Login</button>
+        </nav>
+    );
+};
+
+export default Navbar;
